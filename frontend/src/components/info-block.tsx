65ac9c4c47babb116a1ccaf2683ceaa7faf2f99c
Mayas Faraj <uniqueprogrammer@hotmail.com>
2023-06-14 12:45:10 +0300

    add readmore link

--- frontend/src/components/info-block.tsx
+++ frontend/src/components/info-block.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
 import { SvgIconProps } from "@mui/material";
 import { AccountCircle, AdminPanelSettings, HourglassBottom, ListAlt, Redeem, RoomPreferences } from "@mui/icons-material";
+import ReadMoreIcon from "@mui/icons-material/ReadMore";
 import getServerData from "../libs/server-data";
 import styles from "../styles/info-block.module.scss";
 
@@ -10,14 +12,16 @@ interface IInfoBlockProps {
     icon: React.ReactElement<SvgIconProps>
     description?: string
     isWide?: boolean
+    url?: string
 }
 
-const InfoBlock: FunctionComponent<IInfoBlockProps> = ({ title, description, info, icon, isWide }) => {
+const InfoBlock: FunctionComponent<IInfoBlockProps> = ({ title, description, info, icon, isWide, url }) => {
     return (
         <div className={styles.wrapper + (isWide === true ? " " + styles.wide : "")}>
+            { url != null && <Link to={url ?? '#'} className={styles["readmore-link"]}><ReadMoreIcon /></Link>}
             <div className={styles.icon_wrapper}>{icon}</div>
             <div className={styles.info_wrapper}>
-                <strong className={styles.title}>{title}</strong>
+                <strong className={styles.title}><Link to={url ?? '#'}>{title}</Link></strong>
                 <p className={styles.info}>{info}</p>
                 {description != null && <p className={styles.description}>{description}</p>}
             </div>
@@ -44,6 +48,7 @@ export const AdminInfoBlock = () => {
                 icon: <AdminPanelSettings />,
                 title: "System Admins",
                 info: `${result.data.adminsCount.count}  admins`,
+                url: "/admins",
                 description: `Admin can create admins, sales man, technical user and product manager users, 
             last admin created to system at: ${new Date(parseInt(result.data.adminsCount.date)).toLocaleDateString()}`
             });
@@ -52,6 +57,7 @@ export const AdminInfoBlock = () => {
     }, []);
 
     // render
+
     return <InfoBlock {...info} />
 }
 
@@ -67,6 +73,7 @@ export const ClientInfoBlock = () => {
                 icon: <AccountCircle />,
                 title: "App Clients",
                 info: `${result.data.clientsCount.count}  clients`,
+                url: "/clients",
                 description: `Client who can use the app, 
                 last client registered at: ${new Date(parseInt(result.data.clientsCount.date)).toLocaleDateString()}`
             });
@@ -90,6 +97,7 @@ export const MaintenanceInfoBlock = () => {
                 icon: <RoomPreferences />,
                 title: "Maintenances Schedule",
                 info: `${result.data.maintenancesCount.count} maintenances`,
+                url: "/maintenances",
                 description: `maintenance operations that request from user, 
                 last maintenance registered at: ${new Date(parseInt(result.data.maintenancesCount.date)).toLocaleDateString()}`
             });
@@ -114,6 +122,7 @@ export const OrderInfoBlock = () => {
                 title: "Client Orders",
                 info: `${result.data.ordersCount.sum} ordered products`,
                 isWide: false,
+                url: "/orders",
                 description: `order that generate from user to buy products, there are ${result.data.ordersCount.count} orders,
                 the last order created from client is registered at: ${new Date(parseInt(result.data.ordersCount.date)).toLocaleDateString()}`
             });
@@ -139,6 +148,7 @@ export const ProductInfoBlock = () => {
                 title: "Products",
                 info: `${result.data.productsCount.count} products`,
                 isWide: false,
+                url: "/products",
                 description: `product that generate from user to buy products, 
                 last product registered at: ${new Date(parseInt(result.data.productsCount.date)).toLocaleDateString()}`
             });

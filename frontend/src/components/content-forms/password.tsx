3fc3125c655c4abf4c86f85cd3d3ea40f2301612
uniqueprogrammer <uniqueprogrammer@hotmail.com>
2023-07-31 14:02:59 +0300

    create change password page and components

---
+++ frontend/src/components/content-forms/password.tsx
@@ -0,0 +1,72 @@
+import React, { FunctionComponent } from "react";
+import { FormControl, TextField } from "@mui/material";
+import ContentForm, { reducer } from "../content-form";
+
+const initialInfo = {
+  password: "",
+  confirmPassword: "",
+};
+
+interface IAdminProps {
+  id?: number;
+  onUpdate?: () => void
+}
+
+const Password: FunctionComponent<IAdminProps> = ({ id, onUpdate }) => {
+  // component reducer
+  const [info, dispatch] = React.useReducer(reducer, initialInfo);
+
+  const onFormUpdate = () => {
+    if (onUpdate !== undefined) onUpdate();
+    dispatch({type: "set", key: "password", value: ""});
+    dispatch({type: "set", key: "confirmPassword", value: ""});
+  };
+
+  // process form type (create or update)
+  const adminCommand =
+    id !== undefined
+      ? `mutation { updateAdmin(id: ${id},input: {password: "${info.password}"}) {id user role}}`
+      : `mutation { updateAdminByAuth(input: {password: "${info.password}"}) {id user role}}`;
+
+  // render component
+  return (
+    <ContentForm
+      id={id}
+      name="admin"
+      command={adminCommand}
+      onUpdate={onFormUpdate}
+      commandDisabled={
+        info.password === "" || info.password !== info.confirmPassword
+      }
+    >
+      <FormControl fullWidth margin="normal">
+        <TextField
+          variant="outlined"
+          type="password"
+          label="Password"
+          value={info.password}
+          onChange={(e) =>
+            dispatch({ type: "set", key: "password", value: e.target.value })
+          }
+        />
+      </FormControl>
+      <FormControl fullWidth margin="normal">
+        <TextField
+          variant="outlined"
+          type="password"
+          label="Password"
+          value={info.confirmPassword}
+          onChange={(e) =>
+            dispatch({
+              type: "set",
+              key: "confirmPassword",
+              value: e.target.value,
+            })
+          }
+        />
+      </FormControl>
+    </ContentForm>
+  );
+};
+
+export default Password;
---
+++ frontend/src/pages/password.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Page from "../components/page";
+import Password from "../components/content-forms/password";
+
+const PasswordPage = () => {
+  return (
+    <Page title="Change Password">
+        <Password />
+    </Page>
+  )
+};
+
+export default PasswordPage;

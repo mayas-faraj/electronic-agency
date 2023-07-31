f287522e07a1097b07e5deb6ba76ad43a1292a92
uniqueprogrammer <uniqueprogrammer@hotmail.com>
2023-07-31 14:05:20 +0300

    request notification every 12 seconds

--- frontend/src/components/notification-icon.tsx
+++ frontend/src/components/notification-icon.tsx
@@ -2,63 +2,71 @@ import React, { FunctionComponent } from "react";
 import IconButton from "@mui/material/IconButton";
 import Badge from "@mui/material/Badge";
 import ListAltIcon from "@mui/icons-material/ListAlt";
-import RoomPreferencesIcon from '@mui/icons-material/RoomPreferences';
+import RoomPreferencesIcon from "@mui/icons-material/RoomPreferences";
 import getServerData from "../libs/server-data";
 
 interface NotificationIconProps {
-  icon: JSX.Element
-  count: number
+  icon: JSX.Element;
+  readCount: () => Promise<number>;
 }
 
-const NotificationIcon: FunctionComponent<NotificationIconProps> = ({ icon, count }) => {
+const interval = 12000;
+
+const NotificationIcon: FunctionComponent<NotificationIconProps> = ({
+  icon,
+  readCount,
+}) => {
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    const action = async () => {
+      const countResult = await readCount();
+      setCount(countResult);
+    };
+    action();
+
+    const timer = setInterval(() => {
+      action();
+    }, interval);
+
+    return () => clearInterval(timer);
+  }, [readCount, count]);
   return (
     <IconButton size="large" color="inherit">
-      <Badge badgeContent={count} color="secondary">{icon}</Badge>
+      <Badge badgeContent={count} color="secondary">
+        {icon}
+      </Badge>
     </IconButton>
   );
 };
 
 const OrderNotificationIcon: FunctionComponent = () => {
-  const [count, setCount] = React.useState(0);
-
-  React.useEffect(() => {
-    const action = async () => {
-      const result = await getServerData(`query {
-        ordersUnreadCount {
-          count
-        }
-      }
-      `);
-      setCount(result.data.ordersUnreadCount.count);
-    };
-    action(); 
-  }, []);
+  const readUnreadOrdersCount = async () => {
+    const result = await getServerData(`query { ordersUnreadCount { count } }`);
+    return result.data.ordersUnreadCount.count;
+  };
 
   return (
-    <NotificationIcon icon={<ListAltIcon />} count={count}/>
+    <NotificationIcon
+      icon={<ListAltIcon />}
+      readCount={readUnreadOrdersCount}
+    />
   );
 };
 
 const MaintenanceNotificationIcon: FunctionComponent = () => {
-  const [count, setCount] = React.useState(0);
-
-  React.useEffect(() => {
-    const action = async () => {
-      const result = await getServerData(`query {
-        maintenancesUnreadCount {
-          count
-        }
-      }
-      `);
-      setCount(result.data.maintenancesUnreadCount.count);
-    };
-    action(); 
-  }, []);
+  const readUnreadMaintenancesCount = async () => {
+    const result = await getServerData(`query { maintenancesUnreadCount { count } }`);
+    return result.data.maintenancesUnreadCount.count;
+  };
 
   return (
-    <NotificationIcon icon={<RoomPreferencesIcon />} count={count}/>
+    <NotificationIcon
+      icon={<RoomPreferencesIcon />}
+      readCount={readUnreadMaintenancesCount}
+    />
   );
 };
 
-export { OrderNotificationIcon, MaintenanceNotificationIcon};
+export { OrderNotificationIcon, MaintenanceNotificationIcon };
 export default NotificationIcon;

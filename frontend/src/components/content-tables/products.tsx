8d973b81c24d36720005bab38b877eb013277679
Mayas Faraj <uniqueprogrammer@hotmail.com>
2024-05-07 17:06:06 +0300

    allow to select product

--- frontend/src/components/content-tables/products.tsx
+++ frontend/src/components/content-tables/products.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from "react";
 import { Button, InputAdornment, Modal, TextField } from "@mui/material";
-import { Abc, Search, Visibility } from "@mui/icons-material";
+import { Abc, CheckCircle, Search, Visibility } from "@mui/icons-material";
 import EditIcon from "@mui/icons-material/Edit";
 import Management, { ManagementType, Operation } from "../management";
 import ContentTable, { ITableHeader } from "../content-table";
@@ -13,103 +13,155 @@ import getServerData from "../../libs/server-data";
 
 // types
 interface IProduct {
-    id: number
-    name: string
-    model: string
-    image?: string
-    description?: string
-    price: number
-    isDisabled: boolean
-    createdAt: Date
+  id: number;
+  name: string;
+  model: string;
+  image?: string;
+  description?: string;
+  price: number;
+  isDisabled: boolean;
+  createdAt: Date;
+}
+interface IProductsType {
+  isSelectable?: boolean;
+  onUpdate?: (id: number) => void;
 }
 
 // main component
-const Products: FunctionComponent = () => {
-    // product state
-    const [products, setProducts] = React.useState<IProduct[]>([]);
-    const [editId, setEditId] = React.useState(0);
-    const [viewId, setViewId] = React.useState(0);
-    const [manageItemId, setManageItemId] = React.useState(0);
-    const [keyword, setKeyword] = React.useState("");
+const Products: FunctionComponent<IProductsType> = ({ isSelectable, onUpdate }) => {
+  // product state
+  const [products, setProducts] = React.useState<IProduct[]>([]);
+  const [editId, setEditId] = React.useState(0);
+  const [viewId, setViewId] = React.useState(0);
+  const [manageItemId, setManageItemId] = React.useState(0);
+  const [keyword, setKeyword] = React.useState("");
 
-    // context
-    const privileges = React.useContext(RoleContext);
+  // context
+  const privileges = React.useContext(RoleContext);
 
-    // product schema
-    const tableHeader: ITableHeader[] = [
-        { key: "image", title: "Image", isSpecialType: true },
-        { key: "name", title: "Name" },
-        { key: "model", title: "Model" },
-        { key: "isDisabled", title: "Disable", isControlType: true },
-        { key: "view", title: "More Info", isSpecialType: true },
-        { key: "item", title: "Manage SN", isControlType: true },
-        { key: "edit", title: "Edit", isControlType: true },
-        { key: "delete", title: "Delete", isControlType: true },
-    ];
-    // on load
-    const action = React.useCallback(async () => {
-        const result = await getServerData(`query { products(filter: {showDisabled: true, keyword: "${keyword}"}) { id name model image description price isDisabled }}`)
-        setProducts(result.data.products);
-    }, [keyword]);
+  // product schema
+  const tableHeader: ITableHeader[] = [
+    { key: "image", title: "Image", isSpecialType: true },
+    { key: "name", title: "Name" },
+    { key: "model", title: "Model" },
+    { key: "view", title: "More Info", isSpecialType: true }
+  ];
 
-    // event handler
-    React.useEffect(() => {
-        action();
-    }, [action]);
-
-    // render
-    return (
-        <div>
-            <TextField
-              label="Search"
-              value={keyword}
-              onChange={(e) => { setKeyword(e.target.value) }}
-              variant="outlined"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Search />
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <ContentTable
-                name="product"
-                headers={tableHeader}
-                canRead={privileges.readProduct}
-                canWrite={privileges.writeProduct}
-                hasSnColumn={true}
-                addNewLink="/add-product"
-                isAddNewRight={true}
-                data={
-                    products.map(product => ({
-                        image: <img src={data["site-url"] + product.image} alt={product.name} />,
-                        name: product.name,
-                        model: product.model,
-                        isDisabled: <Management type={ManagementType.switch} operation={Operation.update} command={`mutation { updateProduct(id: ${product.id}, input: {isDisabled: ${!product.isDisabled}})  {id name model}}`} initialValue={product.isDisabled} onUpdate={action} />,
-                        view: <Button variant="text" color="info" onClick={() => setViewId(product.id)}><Visibility /></Button>,
-                        item: <Button variant="text" color="info" onClick={() => setManageItemId(product.id)}><Abc /></Button>,
-                        edit: <Button variant="text" color="success" onClick={() => setEditId(product.id)}><EditIcon /></Button>,
-                        delete: <Management type={ManagementType.button} operation={Operation.delete} command={`mutation { deleteProduct(id: ${product.id})  {id name model}}`} hasConfirmModal={true} onUpdate={action} />
-                    }))
-                } />
-            <Modal open={editId !== 0} onClose={() => setEditId(0)} >
-                <div className="modal">
-                    <ProductForm id={editId} onUpdate={() => action()} />
-                </div>
-            </Modal>
-            <Modal open={viewId !== 0} onClose={() => setViewId(0)} >
-                <div className="modal">
-                    <ProductView id={viewId} />
-                </div>
-            </Modal>
-            <Modal open={manageItemId !== 0} onClose={() => setManageItemId(0)} >
-                <div className="modal">
-                    <ProductItems productId={manageItemId} />
-                </div>
-            </Modal>
-        </div>
+  if (isSelectable) tableHeader.push({ key: "select", title: "select" });
+  else {
+    tableHeader.push(
+      { key: "isDisabled", title: "Disable", isControlType: true },
+      { key: "item", title: "Manage SN", isControlType: true },
+      { key: "edit", title: "Edit", isControlType: true },
+      { key: "delete", title: "Delete", isControlType: true }
     );
+  }
+
+  // on load
+  const action = React.useCallback(async () => {
+    const result = await getServerData(
+      `query { products(filter: {showDisabled: true, keyword: "${keyword}"}) { id name model image description price isDisabled }}`
+    );
+    setProducts(result.data.products);
+  }, [keyword]);
+
+  // event handler
+  React.useEffect(() => {
+    action();
+  }, [action]);
+
+  // render
+  return (
+    <div>
+      <TextField
+        label="Search"
+        value={keyword}
+        onChange={(e) => {
+          setKeyword(e.target.value);
+        }}
+        variant="outlined"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Search />
+            </InputAdornment>
+          )
+        }}
+      />
+      <ContentTable
+        name="product"
+        headers={tableHeader}
+        canRead={privileges.readProduct}
+        canWrite={privileges.writeProduct}
+        hasSnColumn={true}
+        addNewLink="/add-product"
+        isAddNewRight={true}
+        data={products.map((product) => ({
+          image: <img src={data["site-url"] + product.image} alt={product.name} />,
+          name: product.name,
+          model: product.model,
+          isDisabled:
+            isSelectable !== true ? (
+              <Management
+                type={ManagementType.switch}
+                operation={Operation.update}
+                command={`mutation { updateProduct(id: ${product.id}, input: {isDisabled: ${!product.isDisabled}})  {id name model}}`}
+                initialValue={product.isDisabled}
+                onUpdate={action}
+              />
+            ) : undefined,
+          view: (
+            <Button variant="text" color="info" onClick={() => setViewId(product.id)}>
+              <Visibility />
+            </Button>
+          ),
+          item:
+            isSelectable !== true ? (
+              <Button variant="text" color="info" onClick={() => setManageItemId(product.id)}>
+                <Abc />
+              </Button>
+            ) : undefined,
+          edit:
+            isSelectable !== true ? (
+              <Button variant="text" color="success" onClick={() => setEditId(product.id)}>
+                <EditIcon />
+              </Button>
+            ) : undefined,
+          delete:
+            isSelectable !== true ? (
+              <Management
+                type={ManagementType.button}
+                operation={Operation.delete}
+                command={`mutation { deleteProduct(id: ${product.id})  {id name model}}`}
+                hasConfirmModal={true}
+                onUpdate={action}
+              />
+            ) : undefined,
+          select:
+            isSelectable && onUpdate !== undefined ? (
+              <Button variant="text" color="info" onClick={() => onUpdate(product.id)}>
+                <CheckCircle />
+              </Button>
+            ) : undefined
+        }))}
+      />
+      <Modal open={editId !== 0} onClose={() => setEditId(0)}>
+        <div className="modal">
+          <ProductForm id={editId} onUpdate={() => action()} />
+        </div>
+      </Modal>
+      <Modal open={viewId !== 0} onClose={() => setViewId(0)}>
+        <div className="modal">
+          <ProductView id={viewId} />
+        </div>
+      </Modal>
+      <Modal open={manageItemId !== 0} onClose={() => setManageItemId(0)}>
+        <div className="modal">
+          <ProductItems productId={manageItemId} />
+        </div>
+      </Modal>
+    </div>
+  );
 };
 
 export default Products;

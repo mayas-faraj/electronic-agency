ddebccb0f0f23225b358922b02b641507d16913a
Mayas Faraj <uniqueprogrammer@hotmail.com>
2024-05-15 23:20:29 +0300

    add fields for warrany, terms, company and delievery for form and join name by first name and last name

--- frontend/src/components/content-forms/receipt.tsx
+++ frontend/src/components/content-forms/receipt.tsx
@@ -4,7 +4,7 @@ import { Button } from "@mui/material";
 import { CloudDownload, Send } from "@mui/icons-material";
 import { reducer } from "../content-form";
 import logoImage from "../../assets/imgs/logo-receipt.png";
-import greeImage from "../../assets/imgs/gree-logo.webp";
+import greeImage from "../../assets/imgs/gree-logo.jpg";
 import getServerData from "../../libs/server-data";
 import { font } from "../../libs/font";
 import styles from "../../styles/receipt.module.scss";
@@ -13,6 +13,8 @@ const initialInfo = {
   user: "",
   phone: "",
   email: "",
+  firstName: "",
+  lastName: "",
   count: 0,
   offerPrice: 0,
   validationDays: 1,
@@ -57,11 +59,13 @@ const Receipt: FunctionComponent<IReceiptProps> = ({ id, isArabic, onUpdate }) =
   React.useEffect(() => {
     const loadReceipt = async () => {
       const result = await getServerData(
-        `query { order(id: ${id}) { id count address note createdAt product { id name model } client { id user phone email } offer { id price validationDays } } }`
+        `query { order(id: ${id}) { id count address note company warranty delivery terms createdAt product { id name model } client { id user phone email firstName lastName } offer { id price validationDays } } }`
       );
 
       dispatch({ type: "set", key: "phone", value: result.data.order.client.phone });
       dispatch({ type: "set", key: "email", value: result.data.order.client.email });
+      dispatch({ type: "set", key: "firstName", value: result.data.order.client.firstName });
+      dispatch({ type: "set", key: "lastName", value: result.data.order.client.lastName });
       dispatch({ type: "set", key: "user", value: result.data.order.user });
       dispatch({ type: "set", key: "count", value: result.data.order.count });
       dispatch({ type: "set", key: "createdAt", value: result.data.order.createdAt });
@@ -69,6 +73,10 @@ const Receipt: FunctionComponent<IReceiptProps> = ({ id, isArabic, onUpdate }) =
       dispatch({ type: "set", key: "validationDays", value: result.data.order.offer.validationDays ?? 1 });
       dispatch({ type: "set", key: "address", value: result.data.order.address });
       dispatch({ type: "set", key: "note", value: result.data.order.note });
+      dispatch({ type: "set", key: "company", value: result.data.order.company });
+      dispatch({ type: "set", key: "delivery", value: result.data.order.delivery });
+      dispatch({ type: "set", key: "warranty", value: result.data.order.warranty });
+      dispatch({ type: "set", key: "terms", value: result.data.order.terms });
       dispatch({ type: "set", key: "productName", value: result.data.order.product.name });
       dispatch({ type: "set", key: "OfferPrice", value: result.data.order.product.offer?.price ?? 0 });
 
@@ -80,7 +88,7 @@ const Receipt: FunctionComponent<IReceiptProps> = ({ id, isArabic, onUpdate }) =
   // render component
   return (
     <>
-      <div className={`${styles.wrapper} ${isArabic ? "rtl" : ""}`} id="print">
+      <div className={`${styles.wrapper} ${isArabic ? styles.rtl : ""}`} id="print">
         <div>
           <img src={logoImage} alt="logo" className={styles.logo} />
         </div>
@@ -89,30 +97,32 @@ const Receipt: FunctionComponent<IReceiptProps> = ({ id, isArabic, onUpdate }) =
           {!isArabic ? "Offer" : "عرض"} #EST-${id}
         </div>
         <table className={styles.schemaTable}>
-          <tr>
-            <td>{!isArabic ? "Compay:" : "السادة:"}</td>
-            <td>{info.user as string}</td>
-            <td></td>
-            <td></td>
-            <td>{!isArabic ? "Offer Date:" : "تاريخ التقديم:"}</td>
-            <td>{new Date(parseInt(info.createdAt as string)).toLocaleDateString()}</td>
-          </tr>
-          <tr>
-            <td>{!isArabic ? "Att:" : "حضرة السيد:"}</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>{!isArabic ? "Expiry Date:" : "تاريخ انتهاء الصلاحية:"}</td>
-            <td>{new Date(parseInt(info.createdAt as string) + (info.validationDays as number) * 24 * 60 * 60 * 1000).toLocaleDateString()}</td>
-          </tr>
-          <tr>
-            <td>{!isArabic ? "Mobile:" : "رقم الهاتف:"}</td>
-            <td>{info.phone as string}</td>
-            <td></td>
-            <td></td>
-            <td>{!isArabic ? "Reference #:" : "رقم المشروع:"}</td>
-            <td>{id}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>{!isArabic ? "Compay:" : "السادة:"}</td>
+              <td>{info.company as string}</td>
+              <td></td>
+              <td></td>
+              <td>{!isArabic ? "Offer Date:" : "تاريخ التقديم:"}</td>
+              <td>{new Date(parseInt(info.createdAt as string)).toLocaleDateString()}</td>
+            </tr>
+            <tr>
+              <td>{!isArabic ? "Att:" : "حضرة السيد:"}</td>
+              <td>{`${info.firstName}${info.lastName && " "}${info.lastName}`}</td>
+              <td></td>
+              <td></td>
+              <td>{!isArabic ? "Expiry Date:" : "تاريخ انتهاء الصلاحية:"}</td>
+              <td>{new Date(parseInt(info.createdAt as string) + (info.validationDays as number) * 24 * 60 * 60 * 1000).toLocaleDateString()}</td>
+            </tr>
+            <tr>
+              <td>{!isArabic ? "Mobile:" : "رقم الهاتف:"}</td>
+              <td>{info.phone as string}</td>
+              <td></td>
+              <td></td>
+              <td>{!isArabic ? "Reference #:" : "رقم المشروع:"}</td>
+              <td>{id}</td>
+            </tr>
+          </tbody>
         </table>
         <p>
           {!isArabic
@@ -120,57 +130,63 @@ const Receipt: FunctionComponent<IReceiptProps> = ({ id, isArabic, onUpdate }) =
             : "يسرنا أن نقدم لكم عرض السعر التجاري للمنتجات المبينة تفاصيلها أدناه"}
         </p>
         <table className={styles.productsTable}>
-          <tr>
-            <th>{!isArabic ? "#" : "بند"}</th>
-            <th>{!isArabic ? "Item" : "رمز الجهاز"}</th>
-            <th>{!isArabic ? "Description" : "الوصف"}</th>
-            <th>{!isArabic ? "QTY" : "الكمية"}</th>
-            <th>{!isArabic ? "Unit Price" : "السعر"}</th>
-            <th>{!isArabic ? "Amount" : "المبلغ"}</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>{info.productName as string}</td>
-            <td>{info.productModel as string}</td>
-            <td>{info.count as number}</td>
-            <td>{info.offerPrice as number}</td>
-            <td>{(info.offerPrice as number) * (info.count as number)}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <th>{!isArabic ? "#" : "بند"}</th>
+              <th>{!isArabic ? "Item" : "رمز الجهاز"}</th>
+              <th>{!isArabic ? "Description" : "الوصف"}</th>
+              <th>{!isArabic ? "QTY" : "الكمية"}</th>
+              <th>{!isArabic ? "Unit Price" : "السعر"}</th>
+              <th>{!isArabic ? "Amount" : "المبلغ"}</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>{info.productName as string}</td>
+              <td>{info.productModel as string}</td>
+              <td>{info.count as number}</td>
+              <td>{info.offerPrice as number}</td>
+              <td>{(info.offerPrice as number) * (info.count as number)}</td>
+            </tr>
+          </tbody>
         </table>
         <div className={styles.summary}>
           <table className={styles.summaryTable}>
-            <tr>
-              <td>{!isArabic ? "Sub total:" : "المجموع الفرعي:"}</td>
-              <td>{(info.offerPrice as number) * (info.count as number)} $</td>
-            </tr>
-            <tr>
-              <td>{!isArabic ? "Discount:" : "الخصم:"}:</td>
-              <td></td>
-            </tr>
-            <tr className={styles.strong}>
-              <td>{!isArabic ? "Total:" : "الإجمالي:"}</td>
-              <td>{(info.offerPrice as number) * (info.count as number)} $</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>{!isArabic ? "Sub total:" : "المجموع الفرعي:"}</td>
+                <td>{(info.offerPrice as number) * (info.count as number)} $</td>
+              </tr>
+              <tr>
+                <td>{!isArabic ? "Discount:" : "الخصم:"}:</td>
+                <td></td>
+              </tr>
+              <tr className={styles.strong}>
+                <td>{!isArabic ? "Total:" : "الإجمالي:"}</td>
+                <td>{(info.offerPrice as number) * (info.count as number)} $</td>
+              </tr>
+            </tbody>
           </table>
         </div>
         <div className={styles.notes}>
           <table className={styles.notesTable}>
-            <tr>
-              <td>{!isArabic ? "Notes" : "ملاحظات:"}</td>
-              <td>{info.note as string}</td>
-            </tr>
-            <tr>
-              <td>{!isArabic ? "Payment terms:" : "شروط الدفع:"}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>{!isArabic ? "Delivery:" : "التسليم:"}</td>
-              <td></td>
-            </tr>
-            <tr>
-              <td>{!isArabic ? "Warranty:" : "الضمان:"}</td>
-              <td></td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>{!isArabic ? "Notes" : "ملاحظات:"}</td>
+                <td>{info.note as string}</td>
+              </tr>
+              <tr>
+                <td>{!isArabic ? "Payment terms:" : "شروط الدفع:"}</td>
+                <td>{info.terms as string}</td>
+              </tr>
+              <tr>
+                <td>{!isArabic ? "Delivery:" : "التسليم:"}</td>
+                <td>{info.delivery as string}</td>
+              </tr>
+              <tr>
+                <td>{!isArabic ? "Warranty:" : "الضمان:"}</td>
+                <td>{info.warranty as string}</td>
+              </tr>
+            </tbody>
           </table>
         </div>
         <div className={styles.footer}>

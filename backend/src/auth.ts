d0d8e3580575f56ed0c48c1b659d28154e8ce9f5
Mayas Faraj <uniqueprogrammer@hotmail.com>
2024-07-10 05:40:38 +0300

    create matched role for ticketing service

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -3,7 +3,6 @@ import jwt from "jsonwebtoken";
 import { GraphQLError } from "graphql";
 import dotenv from "dotenv";
 
-
 // load configutation
 dotenv.config();
 const secret = process.env.SECRET ?? "";
@@ -13,18 +12,18 @@ interface AuthUser {
   id: number;
   nam: string;
   rol: string;
-};
+}
 
 interface ServiceUser {
   name: string;
   role: string;
   sub: string;
   aud?: string;
-};
+}
 export interface AppContext {
   prismaClient: PrismaClient;
   user: AuthUser;
-};
+}
 
 // check auth function
 export enum Role {
@@ -35,8 +34,13 @@ export enum Role {
 }
 
 export const checkAuthorization = (role: string, ...allowedRoles: Role[]) => {
-  for (let allowedRole of allowedRoles) if (role === Role[allowedRole]) return true;
-  throw(new GraphQLError(`${role != "" ? role : "user without a role"} is unauthorized to access resources, only allowd for role indexs: ${allowedRoles}`));
+  for (let allowedRole of allowedRoles)
+    if (role === Role[allowedRole]) return true;
+  throw new GraphQLError(
+    `${
+      role != "" ? role : "user without a role"
+    } is unauthorized to access resources, only allowd for role indexs: ${allowedRoles}`
+  );
 };
 
 // decode user
@@ -45,12 +49,26 @@ export const generateJwtToken = (user: AuthUser) => {
 };
 
 export const generateServiceJwtToken = (user: ServiceUser) => {
+  let matchedRole = user.role;
+  switch (user.role) {
+    case "PRODUCT_MANAGER":
+      matchedRole = "CONTENT_MANAGER";
+      break;
+    case "SALES_MAN":
+      matchedRole = "CONTENT_WRITER";
+      break;
+    case "TECHNICAL":
+      matchedRole = "LOGESTICS_MANAGER";
+      break;
+  }
+
+  user.role = matchedRole;
   return jwt.sign(user, secret);
 };
 
 // verify user
 export const getUserFromJwt = (authorizationToken: string): AuthUser => {
-  const emptyUser = { id: 0, nam: "", rol: ""};
+  const emptyUser = { id: 0, nam: "", rol: "" };
   if (authorizationToken == null || authorizationToken === "") return emptyUser;
   try {
     const result = jwt.verify(authorizationToken, secret);

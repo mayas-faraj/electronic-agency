9a8f08f07218acbf9d003231df04b2c482c15e5e
Mayas Faraj <uniqueprogrammer@hotmail.com>
2024-08-07 16:01:51 +0300

    remove unused import of AccessType

--- frontend/src/pages/add-ticket.tsx
+++ frontend/src/pages/add-ticket.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Page from "../components/page";
 import Ticket from "../components/content-forms/ticket";
-import { AccessType } from "../components/content-tables/tickets";
 
 const AddProductPage = () => {
   return (

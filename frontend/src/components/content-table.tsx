2ccd114408ecd54f7f158b00c1ded67f5ce2e12a
Mayas Faraj <uniqueprogrammer@hotmail.com>
2024-08-14 17:39:13 +0300

    add hidePagination props and hide csv download button for non write admin privileges

--- frontend/src/components/content-table.tsx
+++ frontend/src/components/content-table.tsx
@@ -1,36 +1,51 @@
 import React, { FunctionComponent, ReactNode } from "react";
 import { Link } from "react-router-dom";
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from "@mui/material";
-import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import RoleContext from "./role-context";
 import CsvDownloadButton from "react-json-to-csv";
 import Content from "./content";
 import styles from "../styles/content-table.module.scss";
 
 // types
 export interface ITableHeader {
-  key: string
-  title: string
-  isControlType?: boolean
-  isSpecialType?: boolean
+  key: string;
+  title: string;
+  isControlType?: boolean;
+  isSpecialType?: boolean;
 }
 
 interface IContentTableProps {
-  name: string
-  headers: ITableHeader[]
-  data: Record<string, ReactNode>[]
-  canRead: boolean
-  canWrite: boolean
-  hasSnColumn?: boolean
-  addNewLink?: string
-  isAddNewRight?: boolean
+  name: string;
+  headers: ITableHeader[];
+  data: Record<string, ReactNode>[];
+  canRead: boolean;
+  canWrite: boolean;
+  hasSnColumn?: boolean;
+  addNewLink?: string;
+  isAddNewRight?: boolean;
+  hidePagination?: boolean;
 }
 
 // main component
-const ContentTable: FunctionComponent<IContentTableProps> = ({ name, headers, canRead, canWrite, hasSnColumn, addNewLink, isAddNewRight, data }) => {
+const ContentTable: FunctionComponent<IContentTableProps> = ({
+  name,
+  headers,
+  canRead,
+  canWrite,
+  hasSnColumn,
+  addNewLink,
+  isAddNewRight,
+  hidePagination,
+  data
+}) => {
   // component state
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // component context
+  const privileges = React.useContext(RoleContext);
 
   // event handler
   const handleChangePage = (event: unknown, newPage: number) => {
@@ -43,79 +58,75 @@ const ContentTable: FunctionComponent<IContentTableProps> = ({ name, headers, ca
   };
 
   // process privilege to product headers
-  const visibleHeader = headers.filter(header => canWrite || !header.isControlType);
+  const visibleHeader = headers.filter((header) => canWrite || !header.isControlType);
 
   // prepare export to csv
-  const csvHeader = headers.filter(header => !header.isControlType && !header.isSpecialType);
+  const csvHeader = headers.filter((header) => !header.isControlType && !header.isSpecialType);
 
   // render
   return (
     <>
-      {
-        (canWrite && addNewLink != null) && <Link to={addNewLink} className={`${styles.button} ${styles["button--main"]} ${isAddNewRight === true ? styles["button--right"] : ""}`}><AddCircleIcon />Add new {name}</Link>
-      }
+      {canWrite && addNewLink != null && (
+        <Link to={addNewLink} className={`${styles.button} ${styles["button--main"]} ${isAddNewRight === true ? styles["button--right"] : ""}`}>
+          <AddCircleIcon />
+          Add new {name}
+        </Link>
+      )}
       <Paper className={styles.wrapper}>
         <TableContainer>
           <Table stickyHeader>
-            {
-              canRead !== true && (
-                <caption className={styles.caption}>Access deined for this role!</caption>
-              )
-            }
+            {canRead !== true && <caption className={styles.caption}>Access deined for this role!</caption>}
             <TableHead>
               <TableRow>
-                {
-                  hasSnColumn && <TableCell>Sn</TableCell>
-                }
-                {
-                  visibleHeader.map(header => (
-                    <TableCell
-                      key={header.key}
-                      align={header.isControlType || header.isSpecialType ? "center" : "left"}>{header.title}</TableCell>
-                  ))
-                }
+                {hasSnColumn && <TableCell>Sn</TableCell>}
+                {visibleHeader.map((header) => (
+                  <TableCell key={header.key} align={header.isControlType || header.isSpecialType ? "center" : "left"}>
+                    {header.title}
+                  </TableCell>
+                ))}
               </TableRow>
             </TableHead>
             <TableBody>
-              {
-                canRead === true && data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((dataRow, rowIndex) => (
-                  <TableRow key={dataRow.id?.toString() ?? `row${rowIndex}`} >
-                    {
-                      hasSnColumn && <TableCell>{(page * rowsPerPage) + rowIndex + 1}</TableCell>
-                    }
+              {canRead === true &&
+                data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((dataRow, rowIndex) => (
+                  <TableRow key={dataRow.id?.toString() ?? `row${rowIndex}`}>
+                    {hasSnColumn && <TableCell>{page * rowsPerPage + rowIndex + 1}</TableCell>}
                     <Content name={name}>
-                      {
-                        visibleHeader.map(header => (
-                          <TableCell key={header.key + dataRow.id?.toString() ?? `row${rowIndex}`} align={header.isControlType || header.isSpecialType ? "center" : "left"}>
-                            {dataRow[header.key]}
-                          </TableCell>
-                        ))
-                      }
+                      {visibleHeader.map((header) => (
+                        <TableCell key={header.key + dataRow.id?.toString()} align={header.isControlType || header.isSpecialType ? "center" : "left"}>
+                          {dataRow[header.key]}
+                        </TableCell>
+                      ))}
                     </Content>
                   </TableRow>
-                ))
-              }
+                ))}
             </TableBody>
           </Table>
         </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={data.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-        <CsvDownloadButton
-          className={styles.button + " " + styles["button--small"]}
-          headers={csvHeader.map(header => header.title)}
-          data={data.map(item => { return csvHeader.map(header => item[header.key]).filter(item => typeof item === "string" || typeof item === "number") })}
-          filename={name + ".csv"}
-          title={`Export ${name} to excel table and Download`}
-        >
-          <CloudDownloadIcon />
-        </CsvDownloadButton>
+        {!hidePagination && (
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={data.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        )}
+        {privileges.writeAdmin && (
+          <CsvDownloadButton
+            className={styles.button + " " + styles["button--small"]}
+            headers={csvHeader.map((header) => header.title)}
+            data={data.map((item) => {
+              return csvHeader.map((header) => item[header.key]).filter((item) => typeof item === "string" || typeof item === "number");
+            })}
+            filename={name + ".csv"}
+            title={`Export ${name} to excel table and Download`}
+          >
+            <CloudDownloadIcon />
+          </CsvDownloadButton>
+        )}
       </Paper>
     </>
   );

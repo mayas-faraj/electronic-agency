ad7d2622f4f302e833db06a746e0996aad1afcef
Mayas Faraj <uniqueprogrammer@hotmail.com>
2024-09-05 21:07:52 +0300

    remove processing of second token functions

--- frontend/src/libs/storage-manager.ts
+++ frontend/src/libs/storage-manager.ts
@@ -1,22 +1,14 @@
 const tokenName = "at";
-const tokenName2 = `${tokenName}2`;
 
 const StorageManager = {
   save: (token: string) => {
     localStorage.setItem(tokenName, token);
   },
-  save2: (token: string) => {
-    localStorage.setItem(tokenName2, token);
-  },
   get: () => {
     return localStorage.getItem(tokenName);
   },
-  get2: () => {
-    return localStorage.getItem(tokenName2);
-  },
   clear: () => {
     localStorage.setItem(tokenName, "");
-    localStorage.setItem(tokenName2, "");
   },
   hasToken: () => {
     const value = localStorage.getItem(tokenName);

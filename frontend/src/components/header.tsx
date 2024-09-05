df1719908d987dd343014ea21a5ebecffc3943a6
Mayas Faraj <uniqueprogrammer@hotmail.com>
2024-09-05 21:16:04 +0300

    prettier the code

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -25,16 +25,18 @@ const Header: FunctionComponent = () => {
 
   // medel menu
   interface ILink {
-    title: string
-    to: string
-    icon: JSX.Element
+    title: string;
+    to: string;
+    icon: JSX.Element;
   }
+
+  const hidenMenuCondition = !privileges.updateClient && (privileges.createOrder || privileges.createTicket);
 
   const links: ILink[] = [];
   if (privileges.readAdmin) links.push({ title: "Admins", to: "/admins", icon: <AdminPanelSettings /> });
-  if (privileges.readAdmin || (privileges.readClient && !privileges.writeTicket)) links.push({ title: "Clients", to: "/clients", icon: <AccountCircle /> });
-  if (privileges.readAdmin || (privileges.readCategory && !privileges.writeTicket)) links.push({ title: "Categories", to: "/categories", icon: <Category /> });
-  if (privileges.readAdmin || (privileges.readProduct && !privileges.writeTicket)) links.push({ title: "Products", to: "/products", icon: <Redeem /> });
+  if (privileges.readClient && !hidenMenuCondition) links.push({ title: "Clients", to: "/clients", icon: <AccountCircle /> });
+  if (privileges.readCategory && !hidenMenuCondition) links.push({ title: "Categories", to: "/categories", icon: <Category /> });
+  if (privileges.readProduct && !hidenMenuCondition) links.push({ title: "Products", to: "/products", icon: <Redeem /> });
   if (privileges.readAdvertisement) links.push({ title: "Advertisements", to: "/advertisements", icon: <BurstModeRounded /> });
   if (privileges.readOrder) links.push({ title: "Orders", to: "/orders", icon: <ListAlt /> });
   if (privileges.readTicket) links.push({ title: "Tickets", to: "/tickets", icon: <RoomPreferences /> });
@@ -61,41 +63,57 @@ const Header: FunctionComponent = () => {
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right", }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right", }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMenuOpen}
       onClose={handleMenuClose}
       className={styles["context-menu"]}
     >
-      <MenuItem><Link to="/password">Change Password</Link></MenuItem>
+      <MenuItem>
+        <Link to="/password">Change Password</Link>
+      </MenuItem>
       <Divider />
-      <MenuItem><Link to="/login">Logout</Link></MenuItem>
+      <MenuItem>
+        <Link to="/login">Logout</Link>
+      </MenuItem>
     </Menu>
   );
 
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileAnchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right", }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right", }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
       className={styles["mobile-menu"]}
     >
-      {
-        links.map(link => (
-          <MenuItem key={link.title}>{link.icon}<Link to={link.to}>{link.title}</Link></MenuItem>
-        ))
-      }
+      {links.map((link) => (
+        <MenuItem key={link.title}>
+          {link.icon}
+          <Link to={link.to}>{link.title}</Link>
+        </MenuItem>
+      ))}
       {privileges.readOrder && (
-        <MenuItem><OrderNotificationIcon />Orders</MenuItem>
+        <MenuItem>
+          <OrderNotificationIcon />
+          Orders
+        </MenuItem>
       )}
       {privileges.readTicket && (
-        <MenuItem><TicketNotificationIcon />Ticket</MenuItem>
+        <MenuItem>
+          <TicketNotificationIcon />
+          Ticket
+        </MenuItem>
       )}
-      <MenuItem onClick={handleProfileMenuOpen}><IconButton size="large" color="inherit"><AccountCircle /></IconButton>Profile</MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton size="large" color="inherit">
+          <AccountCircle />
+        </IconButton>
+        Profile
+      </MenuItem>
     </Menu>
   );
 
@@ -104,24 +122,26 @@ const Header: FunctionComponent = () => {
       <AppBar position="static">
         <Toolbar className={styles.toolbar}>
           <IconButton>
-            <Link to="/"><img src={logoImage} alt="alardh-alsalba logo" className={styles.logo} /></Link>
+            <Link to="/">
+              <img src={logoImage} alt="alardh-alsalba logo" className={styles.logo} />
+            </Link>
           </IconButton>
           <div className={styles.hide_small}>
             <div className={styles["main-menu"]}>
-              {
-                links.map(link => (
-                  <Link key={link.title} to={link.to}>{link.title}</Link>
-                ))
-              }
+              {links.map((link) => (
+                <Link key={link.title} to={link.to}>
+                  {link.title}
+                </Link>
+              ))}
             </div>
             {privileges.readOrder && <OrderNotificationIcon />}
             {privileges.readTicket && <TicketNotificationIcon />}
-            <IconButton size="large" edge="end" onClick={handleProfileMenuOpen} color="inherit" >
+            <IconButton size="large" edge="end" onClick={handleProfileMenuOpen} color="inherit">
               <AccountCircle />
             </IconButton>
           </div>
           <div className={styles.hide_large}>
-            <IconButton size="large" onClick={handleMobileMenuOpen} color="inherit" >
+            <IconButton size="large" onClick={handleMobileMenuOpen} color="inherit">
               <MoreIcon />
             </IconButton>
           </div>

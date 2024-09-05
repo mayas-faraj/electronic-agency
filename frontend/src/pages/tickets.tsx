793217006085b415dd26f9cd5a185e0eca9624fb
Mayas Faraj <uniqueprogrammer@hotmail.com>
2024-09-05 21:15:03 +0300

    remove access type and only change the header depending on the permissions

--- frontend/src/pages/tickets.tsx
+++ frontend/src/pages/tickets.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Page from "../components/page";
-import Tickets, { AccessType } from "../components/content-tables/tickets";
+import Tickets from "../components/content-tables/tickets";
 import RoleContext from "../components/role-context";
 
 const TicketsPage = () => {
@@ -8,36 +8,11 @@ const TicketsPage = () => {
 
   return (
     <Page title="Tickets" hideBack={true}>
-      {privileges.writeAdmin && (
-        <>
-          <h2>All Tickets</h2>
-          <Tickets accessType={AccessType.FULL_ACCESS} />
-        </>
-      )}
-      {privileges.addTicket && (
-        <>
-          <h2>My Tickets</h2>
-          <Tickets accessType={AccessType.READ_ACCESS} />
-        </>
-      )}
-      {!privileges.writeAdmin && privileges.writeTicket && !privileges.writeRepair && !privileges.writeFeedback && !privileges.addTicket && (
-        <>
-          <h2>My Center Tickets</h2>
-          <Tickets accessType={AccessType.UPDATE_ACCESS} />
-        </>
-      )}
-      {!privileges.writeAdmin && privileges.writeRepair && (
-        <>
-          <h2>My Tasks</h2>
-          <Tickets accessType={AccessType.CREATE_ACCESS} />
-        </>
-      )}
-      {!privileges.writeAdmin && privileges.writeFeedback && (
-        <>
-          <h2>My Feedback</h2>
-          <Tickets accessType={AccessType.TWEAK_ACCESS} onlyResolved={true} />
-        </>
-      )}
+      {privileges.createAdmin && <h2>All Tickets</h2>}
+      {!privileges.createAdmin && privileges.createTicket && <h2>My Tickets</h2>}
+      {!privileges.createAdmin && privileges.updateTicket && privileges.updateRepair && privileges.createFeedback && <h2>My Center Tickets</h2>}
+      {!privileges.createAdmin && privileges.createRepair && <h2>My Tasks</h2>}
+      <Tickets />
     </Page>
   );
 };
